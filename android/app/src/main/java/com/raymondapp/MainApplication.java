292cedbef03f473ab0e1801cd1ea4996ccd3f11d
Roman <roman@Romans-Mac.local>
2019-02-20 01:37:20 -0800

    remember

--- android/app/src/main/java/com/raymondapp/MainApplication.java
+++ android/app/src/main/java/com/raymondapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.raymondapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.hopding.pdflib.PDFLibPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new PDFLibPackage(),
             new RNGestureHandlerPackage(),
             new ImagePickerPackage(),
--- src/login/Login.js
+++ src/login/Login.js
@@ -10,145 +10,254 @@ import {
     Alert,
     ActivityIndicator
 } from 'react-native';
+import { CheckBox } from 'react-native-elements'
 import { getDevicePixel, setUser } from '@global';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { AsyncStorage } from "react-native"
 const logo = require('../img/logo1.png');
 const usericon = require('../img/usericon.png');
 
+
 export default class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      loading: false,
-      alertVisible: false,
-      alertContent: '',
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            alertVisible: false,
+            alertContent: '',
+            checked: false
+        }
+        this.rmail = '';
+        this.rpassword = '';
+
+        this.onPressLogin = this.onPressLogin.bind(this);
+        this.signup = this.signup.bind(this);
     }
-    this.onPressLogin = this.onPressLogin.bind(this);
-    this.signup = this.signup.bind(this);
-  }
 
-  componentWillMount() {
-  }
-  
-  componentDidMount() {
-  }
-  
-  componentWillUnmount() {
-
-  }
-
-  signup() {
-    this.props.navigation.navigate('SignupScreen1');
-  }
-
-  onPressLogin() {
-    if (this.state.email == '' || this.state.password == '') {
-        Alert.alert("Please insert \n username or password");
-        console.log(this.state.email);
-        return;
+    componentWillMount() {
+        this.getCheck();
     }
-    this.setState({loading: true});
-    fetch("http://raymondray111.raytax.co.uk/public/api/user/login?email="+this.state.email+"&password="+this.state.password, {
-        method: "POST",
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-       
-      if (responseJson.result === "success") {
-        setUser(responseJson);
-        this.props.navigation.navigate('MainScreen');
-        console.log(this.state.password);
-      } else {
-        Alert.alert("Email or Password is Invalid");
-      } 
-    console.log(responseJson);
-    this.setState({loading: false});
-    }).catch((error) => {
-        this.setState({loading: false});
-        console.log("_______________________________________", error);
-        return;
-      });
-  }
 
-  render() {
-    return (
-        <KeyboardAwareScrollView
-        style={{ backgroundColor: '#fafafa' }}
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        contentContainerStyle={styles.container}
-        scrollEnabled={false}
-        extraScrollHeight={0}
-      >
-        {/* <View style={styles.container}> */}
-            <Image 
-                source = {logo}
-                style = {styles.logo}
-            />
-            <View style={styles.accountInfo}>
-                <Image 
-                    source = {usericon}
-                    style = {styles.usericon}
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+
+    }
+    async getCheck() {
+        try {
+            const value = await AsyncStorage.getItem('remember');
+            if (value === "ture") {
+                console.log("here is console login ", value)
+                await this.getMail();
+                await this.getPassword();
+                console.log(this.rpassword);
+                fetch("http://raymondray111.raytax.co.uk/public/api/user/login?email=" + this.rmail + "&password=" + this.rpassword, {
+                    method: "POST",
+                })
+                    .then((response) => response.json())
+                    .then((responseJson) => {
+
+                        if (responseJson.result === "success") {
+                            setUser(responseJson);
+                            this.props.navigation.navigate('MainScreen');
+                            console.log(this.state.password);
+                        } else {
+                            Alert.alert("Email or Password is Invalid");
+                        }
+                        console.log(responseJson);
+                        this.setState({ loading: false });
+                    }).catch((error) => {
+                        this.setState({ loading: false });
+                        console.log("_______________________________________", error);
+                        return;
+                    });
+
+            } else {
+                console.log("here is sdsfdddddfsdfsdfsdf login ", value)
+            }
+        } catch (error) {
+            // Error retrieving data
+            return;
+        }
+        return value;
+    }
+
+    async setCheck(val) {
+        try {
+            await AsyncStorage.setItem('remember', val);
+        } catch (error) {
+            // Error saving data
+            return;
+        }
+    }
+
+    async setMail(val) {
+        try {
+            await AsyncStorage.setItem('mail', val);
+        } catch (error) {
+            // Error saving data
+            return;
+        }
+    }
+
+    async getMail() {
+        try {
+            this.rmail = await AsyncStorage.getItem('mail');
+            console.log( this.rmail);
+        } catch (error) {
+            // Error saving data
+            return;
+        }
+    }
+
+    async setPassword(val) {
+        try {
+            await AsyncStorage.setItem('password', val);
+        } catch (error) {
+            // Error saving data
+            return;
+        }
+    }
+
+    async getPassword() {
+        try {
+            this.rpassword = await AsyncStorage.getItem('password');
+        } catch (error) {
+            // Error saving data
+            return;
+        }
+    }
+
+    signup() {
+        this.props.navigation.navigate('SignupScreen1');
+    }
+
+    onPressLogin() {
+        if (this.state.email == '' || this.state.password == '') {
+            Alert.alert("Please insert \n username or password");
+            console.log(this.state.email);
+            return;
+        }
+        this.setState({ loading: true });
+
+        if (this.state.checked) {
+            this.setCheck("ture");
+            this.setMail(this.state.email);
+            this.setPassword(this.state.password);
+
+        } else {
+            this.setCheck("false");
+        }
+
+        fetch("http://raymondray111.raytax.co.uk/public/api/user/login?email=" + this.state.email + "&password=" + this.state.password, {
+            method: "POST",
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                if (responseJson.result === "success") {
+                    setUser(responseJson);
+                    this.props.navigation.navigate('MainScreen');
+                    console.log(this.state.password);
+                } else {
+                    Alert.alert("Email or Password is Invalid");
+                }
+                console.log(responseJson);
+                this.setState({ loading: false });
+            }).catch((error) => {
+                this.setState({ loading: false });
+                console.log("_______________________________________", error);
+                return;
+            });
+    }
+
+    render() {
+        return (
+            <KeyboardAwareScrollView
+                style={{ backgroundColor: '#fafafa' }}
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                contentContainerStyle={styles.container}
+                scrollEnabled={false}
+                extraScrollHeight={0}
+            >
+                {/* <View style={styles.container}> */}
+                <Image
+                    source={logo}
+                    style={styles.logo}
                 />
+                <View style={styles.accountInfo}>
+                    <Image
+                        source={usericon}
+                        style={styles.usericon}
+                    />
                     <TextInput
-                    style={styles.email}
-                    placeholder='Email'
-                    placeholderTextColor = "#a6a6a6"
-                    autoCapitalize='none'
-                    maxLength = {40}
-                    keyboardType = "email-address"
-                    ref="email"
-                    onChangeText={(email) => this.setState({email})} 
+                        style={styles.email}
+                        placeholder='Email'
+                        placeholderTextColor="#a6a6a6"
+                        autoCapitalize='none'
+                        maxLength={40}
+                        keyboardType="email-address"
+                        ref="email"
+                        onChangeText={(email) => this.setState({ email })}
                     />
 
                     <TextInput
-                    style={styles.password}
-                    placeholder='Password'
-                    placeholderTextColor = "#a6a6a6"
-                    autoCapitalize='none'
-                    maxLength = {40}
-                    secureTextEntry={true}
-                    ref="password"
-                    onChangeText={(password) => this.setState({password})} 
+                        style={styles.password}
+                        placeholder='Password'
+                        placeholderTextColor="#a6a6a6"
+                        autoCapitalize='none'
+                        maxLength={40}
+                        secureTextEntry={true}
+                        ref="password"
+                        onChangeText={(password) => this.setState({ password })}
                     />
-                <View style={styles.loginDiv}>
-                   { this.state.loading ?
-                        <View
-                        style={styles.signInButton}
-                        onPress={this.onPressLogin}
-                        > 
-                         <ActivityIndicator size="small" />
-                        </View>
-                   :
-                        <TouchableOpacity
-                            style={styles.signInButton}
-                            onPress={this.onPressLogin}
-                        > 
-                        <Text style={styles.signIn}>Sign In</Text>
-                        </TouchableOpacity>
-                    }
+                    <View style={styles.loginDiv}>
+                        {this.state.loading ?
+                            <View
+                                style={styles.signInButton}
+                                onPress={this.onPressLogin}
+                            >
+                                <ActivityIndicator size="small" />
+                            </View>
+                            :
+                            <TouchableOpacity
+                                style={styles.signInButton}
+                                onPress={this.onPressLogin}
+                            >
+                                <Text style={styles.signIn}>Sign In</Text>
+                            </TouchableOpacity>
+                        }
 
-                    {/* <TouchableOpacity
+                        {/* <TouchableOpacity
                         style={[styles.signInButton, {position: 'absolute', right: 0}]}
                         onPress={this.signup}
                     > 
                     <Text style={styles.signIn}>Sign Up</Text>
                     </TouchableOpacity> */}
+                    </View>
+
                 </View>
-            </View>
-            
-            <Text style={styles.note}>
-                Please log in above to send your paperwork to us
+
+                <CheckBox
+                    title='Remember Me'
+                    checked={this.state.checked}
+                    onPress={() => this.setState({ checked: !this.state.checked })}
+                />
+
+                <Text style={styles.note}>
+                    Please log in above to send your paperwork to us
             </Text>
 
-            <TouchableOpacity style={styles.forgotDiv}
-                onPress={()=> this.props.navigation.navigate("ForgotPassScreen")}
-            > 
-                <Text style={styles.forgotText}>Forgot Password?</Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.forgotDiv}
+                    onPress={() => this.props.navigation.navigate("ForgotPassScreen")}
+                >
+                    <Text style={styles.forgotText}>Forgot Password?</Text>
+                </TouchableOpacity>
 
-            {/* <View
+                {/* <View
                 style={{flexDirection: 'row'}}
             >
                 <Text>Please request Raymond’s Accountants</Text>
@@ -159,28 +268,28 @@ export default class Login extends Component {
                     <Text style={{ fontWeight: 'bold', textDecorationLine: 'underline'}}>Sing Up</Text>
                 </TouchableOpacity>
             </View> */}
-        {/* </View> */}
-        </KeyboardAwareScrollView>
-    );
-  }
+                {/* </View> */}
+            </KeyboardAwareScrollView>
+        );
+    }
 }
 
-const styles=StyleSheet.create({
-    container : {
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'white',
-      alignItems: 'center',
-    //   justifyContent: 'center',
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        //   justifyContent: 'center',
     },
     logo: {
         marginTop: '12%',
         width: '70%',
-        height : '25%'
+        height: '25%'
     },
     accountInfo: {
         marginTop: '3%',
-        width : '100%',
+        width: '100%',
         height: '40%',
         backgroundColor: '#0097F5',
         alignItems: 'center',
@@ -225,7 +334,7 @@ const styles=StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    signIn:{
+    signIn: {
         fontSize: getDevicePixel(5),
         fontWeight: 'bold'
     },
@@ -246,7 +355,7 @@ const styles=StyleSheet.create({
         // backgroundColor: 'blue'
     },
     forgotText: {
-        fontSize: 15, 
+        fontSize: 15,
         color: '#7B8D93',
         textDecorationLine: 'underline',
     }
